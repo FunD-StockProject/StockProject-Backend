b81b87fa3c76b6b52cca4987b6297d7291137708
jaewon <hanjw1121@gmail.com>
2024-10-23 22:10:16 +0900

    TEST(#2): 도커 push test

--- src/main/java/com/fund/stockProject/StockProjectApplication.java
+++ src/main/java/com/fund/stockProject/StockProjectApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StockProjectApplication {
-
+	// push test
 	public static void main(String[] args) {
 		SpringApplication.run(StockProjectApplication.class, args);
 	}
