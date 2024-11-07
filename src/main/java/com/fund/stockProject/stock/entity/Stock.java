e3cba0d9273e514b5b10de82f29c19ace3270347
ympark99 <ympark1230@naver.com>
2024-11-07 21:32:23 +0900

    CHORE(#4): swagger 설정 및 아키텍처 기본 설정

---
+++ src/main/java/com/fund/stockProject/stock/entity/Stock.java
@@ -0,0 +1,16 @@
+package com.fund.stockProject.stock.entity;
+
+public class Stock {
+    private Integer id;
+
+    private Integer scoreId;
+
+    private String symbol;
+
+    private String symbolName;
+
+    private String securityName;
+
+    private String exchangeNum;
+
+}
