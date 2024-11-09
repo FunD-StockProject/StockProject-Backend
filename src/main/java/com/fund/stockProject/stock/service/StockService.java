caa32a313e24d33f33edd1e5cc6043869fdd41e0
jerome <alkwen0996@naver.com>
2024-11-09 18:18:39 +0900

    FEAT(#3): 검색어 자동완성 API 로직 구현

--- src/main/java/com/fund/stockProject/stock/service/StockService.java
+++ src/main/java/com/fund/stockProject/stock/service/StockService.java
@@ -2,25 +2,51 @@ package com.fund.stockProject.stock.service;
 
 import com.fund.stockProject.stock.dto.response.StockSearchResponse;
 import com.fund.stockProject.stock.entity.Stock;
+import com.fund.stockProject.stock.repository.StockQueryRepository;
 import com.fund.stockProject.stock.repository.StockRepository;
-
-import org.springframework.stereotype.Service;
-
+import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Service;
 
 @Service
 public class StockService {
 
     private final StockRepository stockRepository;
+    private final StockQueryRepository stockQueryRepository;
 
-    public StockService(StockRepository stockRepository) {
+    public StockService(StockRepository stockRepository, StockQueryRepository stockQueryRepository) {
         this.stockRepository = stockRepository;
+        this.stockQueryRepository = stockQueryRepository;
     }
 
     public StockSearchResponse searchStockBySymbolName(final String symbolName) {
-        final Stock stock = stockRepository.findStockBySymbolName(symbolName).orElseThrow(NoSuchElementException::new);
+        final Stock stock = stockRepository.findStockBySymbolName(symbolName)
+            .orElseThrow(NoSuchElementException::new);
 
         return StockSearchResponse.builder()
+            .stockId(stock.getId())
+            .symbol(stock.getSymbol())
+            .symbolName(stock.getSymbolName())
+            .securityName(stock.getSecurityName())
+            .exchangeNum(stock.getExchangeNum())
+            .scoreId(stock.getScore().getId())
+            .scoreNaver(stock.getScore().getScoreNaver())
+            .scoreReddit(stock.getScore().getScoreReddit())
+            .scoreFinal(stock.getScore().getScoreFinal())
+            .build();
+    }
+
+
+    public List<StockSearchResponse> autoCompleteKeyword(String keyword) {
+        final List<Stock> stocks = stockQueryRepository.autocompleteKeyword(keyword);
+
+        if(stocks.isEmpty()){
+            throw new NoSuchElementException();
+        }
+
+        return stocks.stream()
+            .map(stock -> StockSearchResponse.builder()
                 .stockId(stock.getId())
                 .symbol(stock.getSymbol())
                 .symbolName(stock.getSymbolName())
@@ -30,6 +56,7 @@ public class StockService {
                 .scoreNaver(stock.getScore().getScoreNaver())
                 .scoreReddit(stock.getScore().getScoreReddit())
                 .scoreFinal(stock.getScore().getScoreFinal())
-                .build();
+                .build())
+            .collect(Collectors.toList());
     }
 }
