2cf209e0b31265cf6cfdc4a3831af48a0d33a380
ympark99 <ympark1230@naver.com>
2024-11-14 22:17:46 +0900

    FEAT(#4): WebConfig 추가 설정

--- src/main/java/com/fund/stockProject/global/config/WebConfig.java
+++ src/main/java/com/fund/stockProject/global/config/WebConfig.java
@@ -11,6 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("https://stockvalue13.netlify.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
 }
