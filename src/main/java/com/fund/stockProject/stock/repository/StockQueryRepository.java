ee69fc53e6bb3aeb62ec8a9f41176d49b6cda2fe
jerome <alkwen0996@naver.com>
2024-11-19 23:21:28 +0900

    REFACTOR(#3): 검색어 자동완성 조건 변경

--- src/main/java/com/fund/stockProject/stock/repository/StockQueryRepository.java
+++ src/main/java/com/fund/stockProject/stock/repository/StockQueryRepository.java
@@ -3,6 +3,7 @@ package com.fund.stockProject.stock.repository;
 import com.fund.stockProject.stock.entity.QStock;
 import com.fund.stockProject.stock.entity.Stock;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import java.util.List;
@@ -19,8 +20,17 @@ public class StockQueryRepository {
     }
 
     public List<Stock> autocompleteKeyword(String keyword) {
+        final String[] keywordParts = keyword.split("");
+
+        BooleanExpression condition = null;
+
+        for (String part : keywordParts) {
+            BooleanExpression containsPart = QStock.stock.symbolName.contains(part);
+            condition = (condition == null) ? containsPart : condition.and(containsPart);
+        }
+
         return jpaQueryFactory.selectFrom(QStock.stock)
-            .where(QStock.stock.symbolName.startsWith(keyword))
+            .where(condition)
             .limit(10)
             .fetch();
     }
