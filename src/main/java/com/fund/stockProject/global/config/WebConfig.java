fb53e80671060cffe6c2e567ce94adbe4c38f0c2
ympark99 <ympark1230@naver.com>
2024-11-23 15:23:42 +0900

    CHORE(#20): CORS 설정

--- src/main/java/com/fund/stockProject/global/config/WebConfig.java
+++ src/main/java/com/fund/stockProject/global/config/WebConfig.java
@@ -1,6 +1,5 @@
 package com.fund.stockProject.global.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "https://stockvalue13.netlify.app")
+                .allowedOrigins("http://localhost:5173", "https://stockvalue13.netlify.app", "https://humanzipyo.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
 }
