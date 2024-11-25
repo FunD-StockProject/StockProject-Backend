65a6abd69c9741b1aa2924d89a4cac9ac719d418
ympark99 <ympark1230@naver.com>
2024-11-25 17:40:50 +0900

    FEAT(#12): AI 점수 api 구현

--- src/main/java/com/fund/stockProject/score/repository/ScoreRepository.java
+++ src/main/java/com/fund/stockProject/score/repository/ScoreRepository.java
@@ -5,10 +5,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.fund.stockProject.score.entity.Score;
 
@@ -69,5 +71,10 @@ public interface ScoreRepository extends JpaRepository<Score, Integer> {
      * stock_id와 date로 Score 데이터 조회
      */
     Optional<Score> findByStockIdAndDate(Integer stockId, LocalDate date);
+
+    @Transactional
+    @Modifying // 수정 또는 삭제 쿼리에서 필요
+    @Query("DELETE FROM Score s WHERE s.stockId = :stockId AND s.date = :date")
+    void deleteByStockIdAndDate(@Param("stockId") Integer stockId, @Param("date") LocalDate date);
 
 }
