9c39a611781aed9077a83651e5b1f3828eac9552
jerome <alkwen0996@naver.com>
2025-01-02 17:02:17 +0900

    REFACTOR(#52): 인기 키워드 API 로직 수정 -> 특수기호, 중복제거

--- src/main/java/com/fund/stockProject/keyword/repository/KeywordRepository.java
+++ src/main/java/com/fund/stockProject/keyword/repository/KeywordRepository.java
@@ -1,9 +1,11 @@
 package com.fund.stockProject.keyword.repository;
 
 import com.fund.stockProject.keyword.entity.Keyword;
+import com.fund.stockProject.stock.domain.EXCHANGENUM;
 import com.fund.stockProject.stock.entity.Stock;
 import java.time.LocalDate;
 import java.util.List;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -18,8 +20,13 @@ public interface KeywordRepository extends JpaRepository<Keyword, Integer> {
     @Query("SELECT k FROM Keyword k WHERE k.lastUsedAt < :cutoffDate")
     List<Keyword> findByLastUsedAtBefore(@Param("cutoffDate") LocalDate cutoffDate);
 
-    @Query("SELECT k FROM Keyword k order by frequency DESC limit 9")
-    List<Keyword> findPopularKeyword();
+    @Query("SELECT k " +
+        "FROM StockKeyword sk " +
+        "JOIN sk.stock s " +
+        "JOIN sk.keyword k " +
+        "WHERE s.exchangeNum IN :exchanges " +
+        "ORDER BY k.frequency DESC")
+    List<Keyword> findPopularKeyword(@Param("exchanges") List<EXCHANGENUM> exchanges, Pageable pageable);
 
     @Query("SELECT k FROM StockKeyword sk " +
         "JOIN sk.stock s " +
--- src/main/java/com/fund/stockProject/keyword/service/KeywordService.java
+++ src/main/java/com/fund/stockProject/keyword/service/KeywordService.java
@@ -1,16 +1,18 @@
 package com.fund.stockProject.keyword.service;
 
-import com.fund.stockProject.keyword.dto.PopularKeywordResponse;
 import com.fund.stockProject.keyword.entity.Keyword;
-import java.util.List;
-
-import java.util.stream.Collectors;
-import org.springframework.stereotype.Service;
-
 import com.fund.stockProject.keyword.repository.KeywordRepository;
+import com.fund.stockProject.stock.domain.COUNTRY;
+import com.fund.stockProject.stock.domain.EXCHANGENUM;
 import com.fund.stockProject.stock.entity.Stock;
-
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
@@ -23,15 +25,31 @@ public class KeywordService {
     }
 
 
-    public List<PopularKeywordResponse> findPopularKeyword() {
-        final List<Keyword> popularKeyword = stockKeywordRepository.findPopularKeyword();
+    public List<String> findPopularKeyword(final COUNTRY country) {
+        List<Keyword> popularKeywords = null;
+        Pageable pageable = PageRequest.of(0, 100);
 
-        return popularKeyword.stream()
-            .map(
-                keyword -> PopularKeywordResponse.builder()
-                    .name(keyword.getName())
-                    .freq(keyword.getFrequency())
-                    .build()
-            ).collect(Collectors.toList());
+        if (COUNTRY.KOREA.equals(country)) {
+            List<EXCHANGENUM> exchanges = List.of(EXCHANGENUM.KOSPI, EXCHANGENUM.KOSDAQ, EXCHANGENUM.KOREAN_ETF);
+            popularKeywords = stockKeywordRepository.findPopularKeyword(exchanges, pageable);
+        } else if (COUNTRY.OVERSEA.equals(country)) {
+            List<EXCHANGENUM> exchanges = List.of(EXCHANGENUM.NAS, EXCHANGENUM.NYS, EXCHANGENUM.AMS);
+            popularKeywords = stockKeywordRepository.findPopularKeyword(exchanges, pageable);
+        }
+
+        final Set<String> keywordNames = new HashSet<>();
+
+        for (Keyword keyword : popularKeywords) {
+            if (keywordNames.size() >= 9) {
+                break; // 크기 조건을 먼저 확인해 불필요한 작업 방지
+            }
+
+            String name = keyword.getName();
+            if (!keywordNames.contains(name) && name.matches("^[a-zA-Z0-9가-힣\\s]+$")) {
+                keywordNames.add(name);
+            }
+        }
+
+        return new ArrayList<>(keywordNames);
     }
 }
