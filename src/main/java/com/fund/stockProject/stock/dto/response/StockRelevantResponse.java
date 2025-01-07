522f66c4af3a258d24216f82c1c94ad3455b3c1f
jerome <alkwen0996@naver.com>
2025-01-07 13:05:58 +0900

    REFACTOR(#52): 관련 종목 API 키워드 추가

--- src/main/java/com/fund/stockProject/stock/dto/response/StockRelevantResponse.java
+++ src/main/java/com/fund/stockProject/stock/dto/response/StockRelevantResponse.java
@@ -1,22 +1,23 @@
 package com.fund.stockProject.stock.dto.response;
 
+import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class StockRelevantResponse {
     private final Integer stockId;
-
     private final String symbolName;
-
     private final Integer score;
     private final Integer diff;
+    private final List<String> keyword;
 
     @Builder
-    public StockRelevantResponse(Integer stockId, String symbolName, Integer score, Integer diff) {
+    public StockRelevantResponse(Integer stockId, String symbolName, Integer score, Integer diff, List<String> keyword) {
         this.stockId = stockId;
         this.symbolName = symbolName;
         this.score = score;
         this.diff = diff;
+        this.keyword = keyword;
     }
 }
