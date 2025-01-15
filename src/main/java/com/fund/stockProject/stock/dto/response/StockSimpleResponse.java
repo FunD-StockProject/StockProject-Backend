6226e0b8722d96c791802c290ca1bb5f9411279d
ympark99 <ympark1230@naver.com>
2025-01-15 20:06:55 +0900

    FEAT(#45): hot 지표 api diff 값 추가

--- src/main/java/com/fund/stockProject/stock/dto/response/StockSimpleResponse.java
+++ src/main/java/com/fund/stockProject/stock/dto/response/StockSimpleResponse.java
@@ -1,7 +1,5 @@
 package com.fund.stockProject.stock.dto.response;
 
-import com.fund.stockProject.keyword.entity.Keyword;
-import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -14,10 +12,13 @@ public class StockSimpleResponse {
 
     private final Integer score;
 
+    private final Integer diff;
+
     @Builder
-    public StockSimpleResponse(Integer stockId, String symbolName, Integer score) {
+    public StockSimpleResponse(Integer stockId, String symbolName, Integer score, Integer diff) {
         this.stockId = stockId;
         this.symbolName = symbolName;
         this.score = score;
+        this.diff = diff;
     }
 }
